a5b410b2922743329157dd4fb4387ae89a2d1d69
DayaneCJM <144066003+DayaneCJM@users.noreply.github.com>
2026-04-09 19:02:46 +0000

    aula 09/04

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
+import Hero from "./Components/hero/Hero";
+import Services from "./Components/services/Services";
+import Portifolio from "./Components/portifolio/Portifolio";
+import Contact from "./Components/contact/Contact";
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className="container">
+      <section id="#hero">
+      <Hero/>
+      </section>
+
+      <section id = "#services"> 
+        <Services/>
+      </section>
+
+      <section id = "#portifolio" >
+          <Portifolio/>
+       </section>
+      
+      <section id = "#contact">
+         <Contact/>
+      </section> 
+
+    </div>
   )
 }
 
